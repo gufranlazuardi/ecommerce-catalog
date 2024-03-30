6224ada45fc6cc3036e31ad1771970e74788ddc3
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-31 01:21:32 +0700

    refactor: Card

--- src/components/CardAllProducts.vue
+++ src/components/CardAllProducts.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { RouterLink } from "vue-router";
 import { defineProps } from "vue";
 
@@ -7,13 +7,15 @@ const props = defineProps(["product"]);
 
 <template>
   <router-link :to="`/products/${product.id}`">
-    <div class="flex flex-col border rounded-lg p-4 gap-4 justify-between w-full h-fit">
+    <div
+      class="flex flex-col border rounded-lg px-4 py-6 gap-4 justify-between h-full hover:bg-slate-200 transition-colors duration-500"
+    >
       <div class="flex justify-center">
         <img :src="product.image" :alt="`${title}`" class="w-20 h-20" />
       </div>
       <div class="flex flex-col gap-2">
         <p class="text-sm">{{ product.title }}</p>
-        <p class="font-bold">{{ product.price }}</p>
+        <p class="font-bold">$ {{ product.price }}</p>
       </div>
     </div>
   </router-link>
--- src/components/CardProductDetail.vue
+++ src/components/CardProductDetail.vue
@@ -1,11 +1,16 @@
-<script>
+<script setup>
 import Separator from "../components/ui/separator/Separator.vue";
+import { defineProps } from "vue";
 
 export default {
   components: {
     Separator,
   },
 };
+
+const props = defineProps({
+  detail: Object,
+});
 </script>
 
 <template>
@@ -14,12 +19,12 @@ export default {
       <img src="https://pngimg.com/uploads/dress/small/dress_PNG10.png" alt="dress" class="w-3/4 h-3/4" />
       <div class="flex flex-col gap-2">
         <h1 class="text-[28px] font-semibold text-[#720060]">
-          Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket
+          {{ detail.title }}
         </h1>
         <div class="flex justify-between mt-2">
-          <p class="text-[18px] font-light">women's clothing</p>
+          <p class="text-[18px] font-light">{{ detail.category }}</p>
           <div class="flex items-center gap-2">
-            <p class="text-[18px] font-light">2.9/5</p>
+            <p class="text-[18px] font-light">{{ detail.rating.rate }}</p>
             <div class="flex gap-0.5">
               <div class="w-4 h-4 bg-[#720060] rounded-full"></div>
               <div class="w-4 h-4 bg-[#720060] rounded-full"></div>
@@ -31,12 +36,10 @@ export default {
         </div>
         <Separator />
         <p class="text-[18px] pb-12 mt-2">
-          100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for
-          style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on
-          waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON
+          {{ detail.description }}
         </p>
         <Separator />
-        <p class="text-[28px] text-[#720060] font-semibold">$29</p>
+        <p class="text-[28px] text-[#720060] font-semibold">{{ detail.price }}</p>
         <div class="flex gap-2">
           <div class="w-1/2 h-[42px] border bg-[#720060] text-center text-white py-2 rounded-md">Buy Now</div>
           <div class="w-1/2 h-[42px] border text-center py-2 rounded-md border-[#720060] text-[#720060]">
