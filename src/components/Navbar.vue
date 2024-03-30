4f0169815a14edb04725e4538b37d32d4499965d
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-31 01:23:01 +0700

    refactor: navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,47 @@
-<script>
+<script setup lang="ts">
 import { Button } from "./ui/button";
+import { Search } from "lucide-vue-next";
+import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { ref } from "vue";
 
-export default {
-  components: {
-    Button,
-  },
-};
+const searchTerm = ref("");
 </script>
 
 <template>
-  <div class="flex justify-between">
+  <div class="flex justify-between items-center top-0 z-1000 bg-white sticky h-[60px]">
     <router-link to="/">
       <p class="text-2xl font-semibold">Vogue</p>
     </router-link>
-    <Button>Login</Button>
+
+    <!-- manggil sheet responsive -->
+    <Sheet>
+      <SheetTrigger>
+        <div class="flex-col sm:flex md:flex lg:flex xl:hidden">
+          <div class="w-8 h-1 border border-black bg-black rounded-sm mb-1"></div>
+          <div class="w-6 h-1 border border-black bg-black rounded-sm mb-1"></div>
+          <div class="w-4 h-1 border border-black bg-black rounded-sm"></div>
+        </div>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader class="text-left">Hello!</SheetHeader>
+        <SheetDescription>Please login to have a better experience</SheetDescription>
+        <SheetHeader>
+          <div class="flex flex-col gap-2 mt-6">
+            <Button variant="outline" class="h-9 w-3/4 text-sm">Login</Button>
+            <Button class="h-9 w-3/4 text-sm">Register</Button>
+          </div>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+    <div class="gap-4 hidden xl:flex">
+      <router-link to="/login">
+        <Button variant="outline" class="h-9 text-sm">Login</Button>
+      </router-link>
+      <router-link to="/register">
+        <Button class="h-9 text-sm">Register</Button>
+      </router-link>
+    </div>
   </div>
 </template>
