1223ab1da35ec09b809a053d8cc8215f63859867
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-31 01:23:12 +0700

    feat: skeleton all products

---
+++ src/components/SkeletonAllProducts.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Skeleton } from "@/components/ui/skeleton";
+</script>
+
+<template>
+  <div class="grid grid-cols-5 gap-4 mt-[5rem]">
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+    <Skeleton class="w-full h-[200px] rounded-lg" />
+  </div>
+</template>
