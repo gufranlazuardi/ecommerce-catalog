6a28bfa6933ad553d3fbb9c1f24a417d029b6e8b
gufranlazuardi <gufranlazuardi@gmail.com>
2024-04-01 21:24:58 +0700

    feat: skeleton detail

---
+++ src/components/SkeletonDetail.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Skeleton } from "@/components/ui/skeleton";
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <Skeleton class="w-5/6 h-[30rem] rounded-lg" />
+  </div>
+</template>
