874f93bf8786d72c8903ddbd2dcc73ccc3f58cfb
gufranlazuardi <gufranlazuardi@gmail.com>
2024-04-01 21:25:19 +0700

    fix: all products page

--- src/pages/AllProducts.vue
+++ src/pages/AllProducts.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import SkeletonAllProducts from "../components/SkeletonAllProducts.vue";
 import CardAllProducts from "../components/CardAllProducts.vue";
 import Layout from "../components/Layout.vue";
-import { Input } from "@/components/ui/input";
-import { Search } from "lucide-vue-next";
 import { onMounted, ref } from "vue";
 import axios from "axios";
 
@@ -35,30 +32,6 @@ onMounted(() => {
     <!-- when loading false / fetching -->
     <div class="gap-4 flex justify-center mt-10" v-if="products">
       <!-- category -->
-      <div class="w-fit h-10 p-2 justify-center flex items-center">
-        <Popover>
-          <PopoverTrigger>
-            <p class="text-sm">Category</p>
-          </PopoverTrigger>
-          <PopoverContent>
-            <p class="cursor-pointer p-2 text-sm hover:bg-slate-100 hover:transition-colors hover:p-2">
-              Men's Clothing
-            </p>
-            <p class="cursor-pointer p-2 text-sm hover:bg-slate-100 hover:transition-colors hover:p-2">
-              Women's Clothing
-            </p>
-            <p class="cursor-pointer p-2 text-sm hover:bg-slate-100 hover:transition-colors hover:p-2">Jewelery</p>
-            <p class="cursor-pointer p-2 text-sm hover:bg-slate-100 hover:transition-colors hover:p-2">Electronics</p>
-          </PopoverContent>
-        </Popover>
-      </div>
-      <!-- search -->
-      <div class="relative w-3/4 items-center">
-        <Input id="search" type="text" placeholder="Find your cozy goods here..." class="pl-10 placeholder:italic" />
-        <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
-          <Search class="size-4 text-muted-foreground" />
-        </span>
-      </div>
     </div>
     <div
       class="grid grid-cols-2 gap-2 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
