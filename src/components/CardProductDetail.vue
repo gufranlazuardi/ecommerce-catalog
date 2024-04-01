2a327a686bbb1c14e752af3227bad8f05e4e02de
gufranlazuardi <gufranlazuardi@gmail.com>
2024-04-01 21:24:47 +0700

    feat: card product detail

--- src/components/CardProductDetail.vue
+++ src/components/CardProductDetail.vue
@@ -3,7 +3,7 @@ import Separator from "../components/ui/separator/Separator.vue";
 import { defineProps } from "vue";
 
 const props = defineProps({
-  detail: Object,
+  product: Object, // Define prop type as object
 });
 </script>
 
@@ -13,12 +13,12 @@ const props = defineProps({
       <img src="https://pngimg.com/uploads/dress/small/dress_PNG10.png" alt="dress" class="w-3/4 h-3/4" />
       <div class="flex flex-col gap-2">
         <h1 class="text-[28px] font-semibold text-[#720060]">
-          {{ detail.title }}
+          {{ product.title }}
         </h1>
         <div class="flex justify-between mt-2">
-          <p class="text-[18px] font-light">{{ detail.category }}</p>
+          <p class="text-[18px] font-light">{{ product.category }}</p>
           <div class="flex items-center gap-2">
-            <p class="text-[18px] font-light">{{ detail.rating.rate }}</p>
+            <p class="text-[18px] font-light">{{ product.rating.rate }}</p>
             <div class="flex gap-0.5">
               <div class="w-4 h-4 bg-[#720060] rounded-full"></div>
               <div class="w-4 h-4 bg-[#720060] rounded-full"></div>
@@ -30,10 +30,10 @@ const props = defineProps({
         </div>
         <Separator />
         <p class="text-[18px] pb-12 mt-2">
-          {{ detail.description }}
+          {{ product.description }}
         </p>
         <Separator />
-        <p class="text-[28px] text-[#720060] font-semibold">{{ detail.price }}</p>
+        <p class="text-[28px] text-[#720060] font-semibold">{{ product.price }}</p>
         <div class="flex gap-2">
           <div class="w-1/2 h-[42px] border bg-[#720060] text-center text-white py-2 rounded-md">Buy Now</div>
           <div class="w-1/2 h-[42px] border text-center py-2 rounded-md border-[#720060] text-[#720060]">
